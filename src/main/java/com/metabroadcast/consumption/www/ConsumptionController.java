85fa87df5ee4e4348608c9cfe84a823e0d54b7b2
Ben Smith <ben.thesmith@gmail.com>
2010-08-25 15:33:08 +0100

    more cosmetics

--- src/main/java/com/metabroadcast/consumption/www/ConsumptionController.java
+++ src/main/java/com/metabroadcast/consumption/www/ConsumptionController.java
@@ -18,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import twitter4j.TwitterException;
-
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
