45a5d201e51237a17891f76eba587234a1cc6536
Ben Smith <ben.thesmith@gmail.com>
2010-09-01 10:56:25 +0100

    changed test to check parliment

--- src/test/java/com/metabroadcast/AtlasContentStoreTest.java
+++ src/test/java/com/metabroadcast/AtlasContentStoreTest.java
@@ -18,7 +18,7 @@ public class AtlasContentStoreTest {
     
     @Test
     public void shouldRetrieveItemsOnNow() {
-        List<Item> items = store.getItemsOnNow("http://www.bbc.co.uk/services/bbcone/london");
+        List<Item> items = store.getItemsOnNow("http://www.bbc.co.uk/services/parliament");
         assertFalse(items.isEmpty());
     }
 }
