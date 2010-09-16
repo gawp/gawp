62bad2949e1013bcda5968eb65339940e779d2f9
Ben Smith <ben.thesmith@gmail.com>
2010-09-16 14:19:13 +0100

    added caching of userRefs

--- src/main/java/com/metabroadcast/consumption/ConsumedContentProvider.java
+++ src/main/java/com/metabroadcast/consumption/ConsumedContentProvider.java
@@ -4,8 +4,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.media.entity.simple.Description;
