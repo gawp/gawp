c7abdad2e020a3639d86bd5d36637b86dbe97e94
Ben Smith <ben.thesmith@gmail.com>
2010-10-12 16:27:56 +0100

    changed graph background color

--- src/main/java/com/metabroadcast/consumption/punchcard/Punchcard.java
+++ src/main/java/com/metabroadcast/consumption/punchcard/Punchcard.java
@@ -16,7 +16,7 @@ import com.metabroadcast.common.model.SimpleModel;
 public class Punchcard implements SelfModelling {
     
     private static final String IMAGE_URL = "http://chart.apis.google.com/chart?" +
-    		"chs=800x300&chds=-1,24,-1,7,0,10&chf=bg,s,efefef&" +
+    		"chs=800x300&chds=-1,24,-1,7,0,10&chf=bg,s,ffffff&" +
     		"chd=t:0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23" +
     		"0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23," +
     		"0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23," +
